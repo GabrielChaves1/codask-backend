f8a2e1ab2efd381aa52230d2cfb8127c6070979b
droyendev <gabrielclcplays@gmail.com>
2023-06-25 00:29:11 -0300

    add: login and register now receive username

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, HttpStatus, NotFoundException, Post, Res } from "@nestjs/common";
+import { Body, ConflictException, Controller, Get, HttpStatus, NotFoundException, Post, Res, UnauthorizedException } from "@nestjs/common";
 import { Response } from "express";
 
 import * as bcrypt from "bcrypt";
 
-import { UserAuthDto } from "./dtos/user-auth-dto";
+import { AuthRegisterDto } from "./dtos/auth-register.dto";
+import { AuthLoginDto } from "./dtos/auth-login-dto";
 import { UserService } from "../user/user.service";
 import { AuthService } from "./auth.service";
 import { Public } from "./auth.guard";
@@ -18,31 +19,37 @@ export class AuthController {
 
   @Public()
   @Post("register")
-  async register(@Res() res: Response, @Body() {email, password}: UserAuthDto) {
+  async register(@Res() res: Response, @Body() {username, email, password}: AuthRegisterDto) {
     const saltRounds = await bcrypt.genSalt(12);
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     
-    const emailExists = await this.userService.findByEmail(email);
-    if(emailExists) {
-      return res.status(HttpStatus.BAD_REQUEST)
-        .send("O e-mail fornecido já está em uso. Por favor, tente com outro e-mail");
+    const usernameExists = await this.userService.findByUsername(username);
+    if(usernameExists) {
+      throw new ConflictException("Este usuário já existe");
     }
 
-    await this.userService.create(email, hashedPassword);
+    const emailExists = await this.userService.findByEmail(email);
+    if(emailExists) {
+      throw new ConflictException("O e-mail fornecido já está em uso. Por favor, tente com outro e-mail");
+    }
+
+    await this.userService.create(username, email, hashedPassword);
     
     return res.status(HttpStatus.CREATED).send("Usuário criado com sucesso.");
   }
 
   @Public()
   @Post("login")
-  async login(@Res() res: Response, @Body() {email, password}: UserAuthDto) {
-    const user = await this.userService.findByEmail(email);
+  async login(@Res() res: Response, @Body() {username, password}: AuthLoginDto) {
+    const user = await this.userService.findByUsername(username);
     if(!user) {
       throw new NotFoundException("Usuário não encontrado");
     };
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid) return res.status(HttpStatus.BAD_REQUEST).send("Erro ao efetuar login.");
+    const passwordDoesMatch = await bcrypt.compare(password, user.password);
+    if(!passwordDoesMatch) {
+      throw new UnauthorizedException('A senha fornecida está incorreta.');
+    }
 
     const tokens = await this.authService.generateToken(user.id, user.email);
     return res.status(HttpStatus.OK).json(tokens);
